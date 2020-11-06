2ceefa031d920ba602c9b93b190234939401c402
Brandon Chiazza <brandonchiazza@gmail.com>
2020-11-05 20:25:03 -0500

    udpate app.py

--- Model/app.py
+++ Model/app.py
@@ -1,4 +1,4 @@
-#print("Hello Information Architecture")
+print("Successfully running app.py")
 
-file = codecs.open("./Image_Only_Classifier_ImageNet.html", "r", "utf-8")
+file = codecs.open("~/Image_Only_Classifier_ImageNet.html", "r", "utf-8")
 print(file.read())
